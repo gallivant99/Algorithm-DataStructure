cdb26ea1c7004f37ab268f963d249555d450165c
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 22:02:40 +0800

    Update QuickSort_3.java

--- 排序/比较排序/快速排序/QuickSort_3.java
+++ 排序/比较排序/快速排序/QuickSort_3.java
@@ -1,1 +1,43 @@
+// 三向切分快速排序
 
+
+public class QuickSort_3 {
+    
+    public static void sort(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+    }
+    
+    private static void sort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int val = nums[low];
+        int lt = low;
+        int gt = high;
+        int i = low + 1;
+        while (i <= gt) {
+            if (nums[i] < val) {
+                swap(nums, lt, i);
+                lt++;
+                i++;
+            } else if (nums[i] > val) {
+                swap(nums, gt, i);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+        sort(nums, low, lt - 1);
+        sort(nums, gt + 1, high);
+    }
+
+    
+    private static void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
