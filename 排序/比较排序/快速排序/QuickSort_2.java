74f6ed135162d675b5912214992dadd14964a7d4
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 20:45:13 +0800

    Update QuickSort_2.java

--- 排序/比较排序/快速排序/QuickSort_2.java
+++ 排序/比较排序/快速排序/QuickSort_2.java
@@ -1,1 +1,50 @@
+// 双向切分快速排序
 
+
+class QuickSort_2 {
+  
+    public static void sort(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+    }
+
+    private static void sort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int index = partition(nums, low, high);
+        sort(nums, low, index - 1);
+        sort(nums, index + 1, high);
+    }
+
+  
+    private static int partition(int[] nums, int low, int high) {
+        int val = nums[low];
+        int i = low + 1;
+        int j = high;
+        while (true) {
+            while (i < high && nums[i] < val) {
+                i++;
+            }
+            while (j > low && nums[j] > val) {
+                j--;
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+        swap(nums, j, low);
+        return j;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
