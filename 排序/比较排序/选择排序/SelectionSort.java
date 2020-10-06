89eff93353c7c43651992d1bf60901211c5bc48c
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 20:11:30 +0800

    Update SelectionSort.java

--- 排序/比较排序/选择排序/SelectionSort.java
+++ 排序/比较排序/选择排序/SelectionSort.java
@@ -8,8 +8,9 @@ public class SelectionSort {
         for (int i = 0; i < len; i++) {
             int min = i;
             for (int j = i + 1; j < len; j++) {
-                if (nums[min] > nums[j])
+                if (nums[min] > nums[j]) {
                     min = j;
+                }
             }
             swap(nums, min, i);
         }
@@ -17,8 +18,9 @@ public class SelectionSort {
 
 
     private static void swap(int[] nums, int i, int j) {
-        if (i == j)
+        if (i == j) {
             return;
+        }
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
