1b9dc678b7620f9603b454330797a4d8977c5211
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 16:07:46 +0800

    Update QuickSort_1.java

--- 排序/比较排序/快速排序/QuickSort_1.java
+++ 排序/比较排序/快速排序/QuickSort_1.java
@@ -1,1 +1,38 @@
+// 单项切分快速排序
 
+pubLic class QuickSort_1 {
+    
+    public static void sort(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+    }
+    
+    private static void sort(int[] nums, int low, int high) {
+        if (low >= high)
+            return;
+        int index = partition(nums, low, high);
+        sort(nums, low, index - 1);
+        sort(nums, index + 1, high);
+    }
+
+    // 单项切分函数
+    private static int partition(int[] nums, int low, int high) {
+        int val = nums[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (nums[j] <= val) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        if (i == j)
+            return;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
