8f586f04cba74a44b5512e67ac38b20baae8d2d9
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 20:10:55 +0800

    Update InsertionSort.java

--- 排序/比较排序/插入排序/InsertionSort.java
+++ 排序/比较排序/插入排序/InsertionSort.java
@@ -6,17 +6,20 @@ public class InsertionSort {
         int len = nums.length;
         for (int i = 1; i < len; i++) {
             for (int j = i; j > 0; j--) {
-                if (nums[j] < nums[j - 1])
+                if (nums[j] < nums[j - 1]) {
                     swap(nums, j, j - 1);
-                else
+                }
+                else {
                     break;
+                }
             }
         }
     }
 
     private static void swap(int[] nums, int i, int j) {
-        if (i == j)
+        if (i == j) {
             return;
+        }
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
