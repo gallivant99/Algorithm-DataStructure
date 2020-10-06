6dee76f8262062228bd1ec877c6d4c74bd75fdc2
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-06 15:30:09 +0800

    Update BubbleSort.java

--- 排序/比较排序/冒泡排序/BubbleSort.java
+++ 排序/比较排序/冒泡排序/BubbleSort.java
@@ -1,1 +1,30 @@
+// 冒泡排序
 
+
+public class BubbleSort {
+    
+    public static void sort(int[] nums) {
+        int len = nums.length;
+        boolean flag = true;
+        for (int i = len - 1; i > 0; i--) {
+            flag = true;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                    flag = false;
+                }
+            }
+            if (flag)
+                return;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        if (i == j)
+            return;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+}
