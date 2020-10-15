12b3d33530d98f338fdf1e208986818f301f0e68
gallivant99 <62582400+gallivant99@users.noreply.github.com>
2020-10-15 22:22:01 +0800

    Create Solution.java

---
+++ 二分查找/二分查找左边界/Solution.java
@@ -0,0 +1,26 @@
+// 二分查找搜寻目标元素左边界
+
+class Solution {
+
+    public int binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        if (nums[low] != target) {
+            return -1;
+        }
+        
+        return low;
+    }
+    
+}
